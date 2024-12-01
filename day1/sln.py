f1effaad39e38e139fae1a4c6752bd5ccd8ab517
Branislav Jenco <branislav.jenco@sesam.io>
2024-12-01 15:22:00 +0100

    Cleanup

--- day1/sln.py
+++ day1/sln.py
@@ -1,31 +1,25 @@
 from collections import defaultdict
 
-left = []
-right = []
+ls = []
+rs = []
 
 with open("input.txt") as f:
     for line in f.readlines():
         l, r = line.split("   ")
-        left.append(int(l))
-        right.append(int(r))
+        ls.append(int(l))
+        rs.append(int(r))
 
-left = sorted(left)
-right = sorted(right)
+ls = sorted(ls)
+rs = sorted(rs)
 
-res_1 = 0
-for l,r in zip(left, right):
-    res_1 += abs(l - r)
-
+res_1 = sum([abs(l - r) for l,r in zip(ls, rs)])
 print("Part 1", res_1)
 
 right_counts = defaultdict(int)
-for n in right:
+for n in rs:
     right_counts[n] += 1
 
-res_2 = 0
-for n in left:
-    res_2 += n * right_counts[n]
-
+res_2 = sum([n * right_counts[n] for n in ls])
 print("Part 2", res_2)
 
     
