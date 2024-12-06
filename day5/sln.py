af81260a59e04da8c92f7dd837b309cf474d6d7a
Branislav Jenco <branislav.jenco@sesam.io>
2024-12-06 19:57:34 +0100

    Finished day 5 part 2

--- day5/sln.py
+++ day5/sln.py
@@ -1,36 +1,35 @@
 from collections import defaultdict
+from functools import cmp_to_key
 
 rules_s = ""
 updates_s = ""
 
-with open("test_input.txt") as f:
+with open("input.txt") as f:
     rules_s, updates_s = f.read().split("\n\n")
 
-rules = [[int(x) for x in rule.split("|")] for rule in rules_s.split("\n")]
+rules = [tuple([int(x) for x in rule.split("|")]) for rule in rules_s.split("\n")]
 updates = [[int(x) for x in update.split(",")] for update in updates_s.strip().split("\n")]
-print(rules)
-order = defaultdict(list)
-order = []
 
-rules2 = rules + [[y,x] for x,y in rules]
-print(rules2)
+def cmp(a, b):
+    if (a,b) in rules:
+        return -1
+    elif (b,a) in rules:
+        return 1
+    else:
+        return 0
 
-res = 0
+res_1 = 0
+res_2 = 0
 for u in updates:
-    m = {} 
-    for i,n in enumerate(u):
-        m[n] = { "before": u[:i], "after": u[i+1:] }
-    invalid = False
-    for n, _map in m.items():
-        for r in rules:
-            if (r[0] == n and r[1] in _map["before"]) or (r[1] == n and r[0] in _map["after"]):
-                invalid = True
-        if invalid:
-            break
-    if not invalid:
-        res += u[len(u)//2]
+    sorted_u = sorted(u, key=cmp_to_key(cmp))
+    if tuple(u) == tuple(sorted_u):
+        res_1 += u[len(u)//2]
+    else:
+        res_2 += sorted_u[len(sorted_u)//2]
 
-print("Part 1", res)
+
+print("Part 1", res_1)
+print("Part 2", res_2)
         
                 
                 
